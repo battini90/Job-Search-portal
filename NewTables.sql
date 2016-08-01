01a650688020c73fb0b7af1033c959a28c75e6a9
battini90 <koushikbattini@gmail.com>
2016-08-01 07:21:29 -0500

    Update NewTables.sql

--- NewTables.sql
+++ NewTables.sql
@@ -18,3 +18,13 @@ CONSTRAINT Tech_info_pkey PRIMARY KEY (Id),
       REFERENCES technology(id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+-- Insert scripts for Tech_information table
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('spectrum','GR','jr.programmer Analyst','something and something','www.spectrum.com', 1); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('ford','Omaha','sr.programmer Analyst','something and something','www.ford.com', 2); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('General Motors','Michigan','programmer Analyst','something and something','www.GM.com', 3); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('Dematic','Michigan','programmer Analyst','something and something','www.dematic.com', 4); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('JBHunt','Arkansas','programmer Analyst','something and something','www.JBhunt.com', 2); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('Tyson','Michigan','programmer Analyst','something and something','www.Tyson.com', 1); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('Walmart','Arkansas','programmer Analyst','something and something','www.walmart.com', 1); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('West','Nebraska','programmer Analyst','something and something','www.west.com', 1); 
+Insert into tech_information (company_name, state, java_position, job_description, job_url, tech_id)values ('Infosys','Hyd','programmer Analyst','something and something','www.infosys.com', 1); 
